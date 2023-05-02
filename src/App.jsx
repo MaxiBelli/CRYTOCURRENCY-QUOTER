9ae78286167ddd41285550c473434e455159b30a
MaxiBelli <maximiliano_belli@hotmail.com>
2023-05-02 10:09:52 -0300

    Add Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
-import { useState } from 'react'
+import { useState } from "react";
+import styled from "@emotion/styled";
+import CryptoImage from "./img/image-cryptos.png";
 
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`;
 
 function App() {
- 
-
   return (
-   <h1>From App</h1>
-  )
+    <Container>
+      <Image src={CryptoImage} alt="crypto images" />
+      <div>
+        <Heading>Instantly Quote Cryptocurrencies</Heading>
+      </div>
+    </Container>
+  );
 }
 
-export default App
+export default App;
