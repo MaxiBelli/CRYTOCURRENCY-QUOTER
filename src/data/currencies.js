5750d6a3b8f2d5d50b5560660dd7dac73eac696a
MaxiBelli <maximiliano_belli@hotmail.com>
2023-05-04 17:46:43 -0300

    feat: Add more currencies to the list

--- src/data/currencies.js
+++ src/data/currencies.js
@@ -1,9 +1,17 @@
 const currencies = [
     { id: 'USD', name: 'United States Dollar'},
-    { id: 'ARG', name: 'Argentine Peso'},
-    { id: 'MXN', name: 'Mexican Peso'},
     { id: 'EUR', name: 'Euro'},
     { id: 'GBP', name: 'Pound Sterling'},
+    { id: 'CHF', name: 'Swiss Franc'},
+    { id: 'JPY', name: 'Japanese Yen'},
+    { id: 'HKD', name: 'Hong Kong Dollar'},
+    { id: 'CAD', name: 'Canadian Dollar'},
+    { id: 'CNY', name: 'Chinese Yuan'},
+    { id: 'AUD', name: 'Australian Dollar'},
+    { id: 'BRL', name: 'Brazilian Real'},
+    { id: 'RUB', name: 'Russian Ruble'},
+    { id: 'MXN', name: 'Mexican Peso'},
+    { id: 'ARS', name: 'Argentine Peso'},
 ]
 
 export {
